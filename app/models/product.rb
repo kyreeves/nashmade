97ead9c79a182576933da7a8bd079a3a36fe4f4e
Kyle Reeves <kyreeves@icloud.com>
2015-04-12 15:39:17 -0400

    add User and Product association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  belongs_to :user
+
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
