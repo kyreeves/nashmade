b5130100a86544ec970626827cf5feee750368b6
Kyle Reeves <kyreeves@icloud.com>
2015-04-14 18:57:51 -0400

    changed seed gile and product view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-User.create!(name: "Kyle's Good Store", email: "kyle@email.com", password: "password", password_confirmation: "password23", role: "admin")
-User.create!(name: "John Doe", email: "johndoe@email.com", password: "password", password_confirmation: "password23", role: "member")
+User.create!(name: "Kyle's Good Store", email: "kyle@email.com", password: "password", password_confirmation: "password", role: "admin")
+User.create!(name: "John Doe", email: "johndoe@email.com", password: "password", password_confirmation: "password", role: "member")
+User.create!(name: "Person", email: "person@email.com", password: "password", password_confirmation: "password", role: "member")
