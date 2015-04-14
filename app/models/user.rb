877aa782c841acbc91fd702d6709e9fda3620d0f
Kyle Reeves <kyreeves@icloud.com>
2015-04-14 16:43:33 -0400

    add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
+  validates :email, presence: true
+  validates :role, presence: true
+
+  def admin?
+    role == "admin"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-User.create!(email: "kyreeves@icloud.com", password: "password23", password_confirmation: "password23", role: "admin")
+User.create!(name: "Kyle's Good Store", email: "kyle@email.com", password: "password", password_confirmation: "password23", role: "admin")
+User.create!(name: "John Doe", email: "johndoe@email.com", password: "password", password_confirmation: "password23", role: "member")
