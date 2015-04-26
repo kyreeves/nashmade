2dd9249ea0d41334a24689cbea8996a9088ab2ed
Kyle Reeves <kyreeves@icloud.com>
2015-04-25 20:59:36 -0400

    change button color

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  $(".dropdown-button").dropdown({hover: true});
   $(".button-collapse").sideNav();
   $('.card-panel').delay(1500).fadeOut(600);
   $('.slider').slider({full_width: true});
