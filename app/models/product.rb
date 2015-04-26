fbf1ac3d923ed861ee7b83af7d7a0e7223e7fa83
Kyle Reeves <kyreeves@icloud.com>
2015-04-26 15:35:13 -0400

    add image validation for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,11 @@ class Product < ActiveRecord::Base
   has_many :orders, dependent: :destroy
 
   has_attached_file :image,
-                    styles: { large: "500x500", medium: "300x300", thumb: "150x150" },
-                    default_url: "default.jpg"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+                    styles: { large: "500x500", medium: "300x300", thumb: "150x150" }
 
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :category, presence: true
+  validates :image, presence: true
 end
